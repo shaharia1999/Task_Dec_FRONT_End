35874c835ea023d4e7e0aa767053d6d87605d6eb
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-07-25 22:50:23 +0600

    service-add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './compunent/Home/Home';
+import Login from './compunent/Login/Login';
 import Navber from './compunent/Navber/Navber';
+import Services from './compunent/Services/Services';
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Navber></Navber>
      <Routes>
         <Route path="/"element={<Home/>}></Route>
+        <Route path="/service"element={<Services/>}></Route>
+        <Route path="/login"element={<Login/>}></Route>
      </Routes>
      
     </div>
--- src/compunent/Services/Services.js
+++ src/compunent/Services/Services.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Services.css'
 
 const Services = () => {
     return (
-        <div>
+        <div className='services'>
             <h1>This is Shaharia</h1>
             
         </div>
