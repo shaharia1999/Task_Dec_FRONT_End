ad153d94259a9cbb77714e4d0abab737c104f95f
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-07-26 13:51:51 +0600

    log-in

--- src/compunent/Login/Login.js
+++ src/compunent/Login/Login.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
+import { AiFillCaretLeft} from 'react-icons/ai';
+import {AiFillEye} from 'react-icons/ai';
+import {  Link } from "react-router-dom";
+
+
+
 
 const Login = () => {
+  const [pass,setPass]=useState(false);
+  const changes=()=>{
+            setPass(pass=>!pass)
+  }
+  console.log(pass);
     return (
         <div className='login'>
-          <h1>Login Page</h1>
+           <div className='login-grid'>
+            <div className="side-ber">
+              <h1>Globetech <br />Company Limited</h1>
+              <p>Make yourself digitalized & more effecient</p>
+              <button>
+                <Link to='/'>
+                <AiFillCaretLeft size='3rem' color='white'></AiFillCaretLeft>
+                </Link>
+              
+                </button>
+            </div>
+            <div className='login-form'>
+               <form>
+                <h2>Wellcome</h2>
+                <p>Sign in to your account </p>
+                <input type="email" placeholder='email' required/>
+                <br/>
+                <div className='password'>
+                  {pass? <input placeholder='password'type='text' required/>: <input placeholder='password'type='password' required/>}
+                {/* <input placeholder='password'type='password'/> */}
+                <span onClick={changes}>
+                  <AiFillEye color="black" size='2rem'></AiFillEye>
+                </span>
+                </div>
+                <br />
+                <input type='text' value='Login' className='button'/>
+               </form>
+            </div>
+           </div>
             
         </div>
     );
